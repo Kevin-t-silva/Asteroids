86ed77f81107d9ecf8a8613bbedfb577a3abb3dd
kevin.silva <kevin.t.silva@protonmail.com>
2025-12-06 11:27:30 -0300

    feature: adicionado tick para contagem de fps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,7 +17,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(f"{dt}")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
