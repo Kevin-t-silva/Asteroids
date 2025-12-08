3a9f5bdba4e820829857518ef4671ed724058f1f
kevin.silva <kevin.t.silva@protonmail.com>
2025-12-08 12:34:17 -0300

    feature: adicionado colisão e asteroids ao jogo, modificado a classe pai circleshape.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
+
 import pygame
 
+from asteroid import Asteroid
+from asteroidsfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
+from player import Player
 
 
 def main():
@@ -10,15 +15,35 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+        for objects in asteroids:
+            if objects.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
+
         print(f"{dt}")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
